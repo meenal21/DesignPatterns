7335dd9fdd07a847214b9d4a956f8c8796506698
Meenal Jain <mpbjain@gmail.com>
2025-08-31 20:52:25 +0530

    flexibility check

---
+++ factoryPattern/src/main/java/org/example/JEE_shortanswer_eval.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class JEE_shortanswer_eval implements  Evaluator{
+    @Override
+    public void evaluate(String response) {
+        System.out.println("Evaluated "+ response + ": JEE SHORTANSWER EVALUATION SUCCESSFUL");
+    }
+}
